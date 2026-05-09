4d794442b32ad3a6942ffbcdef32682cb160fd8c
Iagoscastro16 <Iagoscastro16@gmail.com>
2026-05-09 10:18:49 -0300

    Adicionando a ativação da descoberta de rede
    Tanto na função nova quanto para o isadmin

--- config.py
+++ config.py
@@ -17,13 +17,27 @@ def desativar_firewall():
                 )
     if resultado.returncode == 0:
         print("[OK]Firewall desativado com sucesso!")
-        input("Digite qualquer tecla para sair... ")
     else:
         print(f"[Erro]{resultado.stderr}")
+        print(resultado.stdout)
+
+def ativar_descoberta_rede():
+    ativResultado = subprocess.run(["powershell", "-Command", "netsh advfirewall firewall set rule group='Descoberta de rede' new enable=Yes"],
+                                   capture_output=True,
+                                   text=True)
+    if ativResultado.returncode == 0:
+        print("Descoberta da Rede ativada com sucesso")
+    else:
+        print(f"[Erro]{ativResultado.stderr}")
+        print(f"[STDOUT]ativResultado.stdout")
 
 if isAdmin():
-    print("Executnado como administrador")
+    print("Executando como administrador")
     desativar_firewall()
+    
+    ativar_descoberta_rede()
+    
+    input("Digite qualquer tecla para sair... ")
 else:
     print("Solicitando acesso como admin")
     ctypes.windll.shell32.ShellExecuteW(None,
